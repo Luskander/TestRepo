0fbe931aeba2d267bbd6cbc8e26a3216611bc74d
Luskander <55799709+Luskander@users.noreply.github.com>
2025-01-16 15:08:43 +0200

    Delete src directory

--- src/App/standard-colors.js
+++
@@ -1,5 +0,0 @@
-export const pink = "#B771E5";
-export const lightBeige = "#F6F3E4";
-export const yellow = "#f9da11";
-export const red = "#f90004";
-export const green = "#21BA45";
--- src/App/test.js
+++
@@ -1,4 +0,0 @@
-import _ from "lodash";
-
-const test = _.replace("Hi Fred Fred Fred", "Fred", "Barney");
-console.log({ test });
